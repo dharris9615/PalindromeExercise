b8c3c63473a15e58111aec936a00b70921ac0072
David Harris <davlsu14@gmail.com>
2021-04-26 14:06:57 -0500

    Completed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("rotator", true)]
+        [InlineData("David", false)]
+        public void Test1(string word, bool expected)
         {
+            //arrange
+            var pal = new WordSmith();
 
+            //act
+            bool actual = pal.IsPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
+
+        
+
     }
 }
